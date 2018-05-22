ad84e5dda7120985bcbea62bd1d96e2b13faac15
RemiLecouillard <remizbay@hotmail.fr>
2018-05-22 09:44:59 +0200

    Adds license

--- src/raceManagerInterface.c
+++ src/raceManagerInterface.c
@@ -1,12 +1,31 @@
-//
-// Created by lecouillard on 18/04/18.
-//
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
 
+/**
+ * @author Rémi Lecouillard
+ * @author Baptiste Dromard
+ * @date 18/04/18
+*/
 
 #include <stdlib.h>
 #include "driver.h"
 #include "raceManagerInterface.h"
-#include "racetrack.h"
 
 struct racemanagerinterface {
     Racetrack racetrack;
